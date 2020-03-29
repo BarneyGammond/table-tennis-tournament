86b741d00069ace3de832e3e00c8189606db90be
Barney Gammond <barney@gammond.co.uk>
2020-03-29 15:30:24 +0100

    Added a reducer and gave it an ADD_PLAYERS case

--- src/Data/initial.js
+++ src/Data/initial.js
@@ -1,6 +1,6 @@
 export default {
 
-    players: {},
+    players: [],
     rounds: {}
 
 }
---
+++ src/Data/reducer.js
@@ -0,0 +1,38 @@
+
+const addPlayers = (state,{playerList}) => {
+
+    // variable below is a formatted version of the player names for the state
+
+    const newPlayerList = playerList.map((player,i) => {
+
+        return {
+
+            id: i+1,
+            name: player,
+            wins: 0,
+            pointsWon: 0,
+            pointsConceded: 0,
+
+        }
+
+    })
+
+    return {
+
+        ...state,
+        Players: newPlayerList
+
+    }
+
+}
+
+
+const reducer = (state,action) => {
+
+    switch(action.type) {
+
+        case "ADD_PLAYERS": return addPlayers(state,action)
+        
+    }
+
+}
