26ef252d56b2c5c304ae6a61138c571bebe0754f
Barney Gammond <barney@gammond.co.uk>
2020-04-01 13:46:39 +0100

    Added a tournament round component that contains the match cards

--- src/Components/TournamentView/Tournament.js
+++ src/Components/TournamentView/Tournament.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import TournamentCard from './TournamentCard'
+import TournamentRound from './TournamentRound'
 import {Container} from 'react-bootstrap'
 
 export default ({rounds,handleResult}) => {
@@ -8,33 +8,14 @@ export default ({rounds,handleResult}) => {
 
         <Container className="tournamentWrapper">
 
-            { rounds.map((round,roundIndex) => {
-
-                //checks if this is the final round
-
-                const final = round.length === 1 ? true : false 
+            { rounds.map((round,roundIndex) => { 
 
                 return (
-                    <Container key={roundIndex} className="cardWrapper">
-
-                        { round.map((match,matchIndex) => (
-
-                            <TournamentCard 
-                                onWinnerClick={handleResult}
-                                played={match.played}
-                                key={matchIndex}
-                                roundIndex={roundIndex}
-                                matchIndex={matchIndex}
-                                player1={ match.p1.name }
-                                player2={ match.p2.name }
-                                player1ID={match.p1.id}
-                                player2ID={match.p2.id}
-                                final={final}  
-                            />
-
-                        ))}
-
-                    </Container>
+                    <TournamentRound 
+                        round={round} 
+                        handleResult={handleResult} 
+                        roundIndex={roundIndex}  
+                    />
                 )
 
             })}
