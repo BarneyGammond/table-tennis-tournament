2abd6b0b7c6d3bfa5f378d6e0bd45efbdac8d436
Barney Gammond <barney@gammond.co.uk>
2020-04-02 13:21:55 +0100

    Score submit now added to the dispatch function

--- src/Components/TournamentView/ScoreInput.js
+++ src/Components/TournamentView/ScoreInput.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container,Form,Button}  from  'react-bootstrap'
 
-export default ({p1Name,p2Name,display}) => {
+export default ({p1Name,p2Name,display,submitScore}) => {
 
     let styling = {
         display: display,
         position: 'absolute',
-        backgroundColor: 'rgba(46,46,49,0.5)'
+        backgroundColor: 'rgba(46,46,49,0.9)'
+    }
+
+    let [p1Score,setp1Score] = useState(0)
+    let [p2Score,setp2Score] = useState(0)
+
+    const handleChange = (e,player) => {
+
+        player === 'p1' ? setp1Score(e.currentTarget.value)
+            : setp2Score(e.currentTarget.value)
+
+        console.log(p1Score);
+        console.log(p2Score)
+
+    }
+
+    const handleClick = () => {
+
+        p1Score >= 21 || p2Score >= 21 ?
+            submitScore(p1Score,p2Score) :
+            alert('One of the players must have scored at least 21 points to win')
+
     }
 
     return (
@@ -15,14 +36,22 @@ export default ({p1Name,p2Name,display}) => {
             <Form>
             
                 <Form.Group>
-                    <Form.Label>{p1Name} Score</Form.Label>
-                    <Form.Control type='number'></Form.Control>
+                    <Form.Label className="text-light">{p1Name} Score</Form.Label>
+                    <Form.Control 
+                        onChange={(e) => handleChange(e,'p1')}
+                        value={p1Score}
+                        type='number'>
+                    </Form.Control>
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label>{p2Name} Score</Form.Label>
-                    <Form.Control type='number'></Form.Control>
+                    <Form.Label className="text-light">>{p2Name} Score</Form.Label>
+                    <Form.Control 
+                        onChange={(e) => handleChange(e,'p2')}
+                        value={p2Score}
+                        type='number'></Form.Control>
                 </Form.Group>
-                <Button>Submit Score</Button>
+
+                <Button onClick={handleClick}>Submit Score</Button>
 
             </Form>
 
--- src/Components/TournamentView/TournamentCard.js
+++ src/Components/TournamentView/TournamentCard.js
@@ -15,42 +15,45 @@ export default ({
 
     const [scoreDisplay,setScoreDisplay] = useState('none')
 
-    const handleClick = (playerNumber) => {
+    const handleClick = () => {
 
-        setScoreDisplay('block')
-
-        //This ensure the losers id is passed on
-
-        /* const loserID = playerNumber === 1 ? player2ID : player1ID
-        const winnerID = playerNumber === 1 ? player1ID : player2ID
-
-        return played 
-            ? null
-            : onWinnerClick(loserID,winnerID,matchIndex,roundIndex) */
+        return played ? null : setScoreDisplay('block')
             
+    }
+
+    const submitScore = (p1Score,p2Score) => {
+
+        let winner = p1Score > p2Score ? player1ID : player2ID
+        let loser = p1Score < p2Score ? player1ID : player2ID
+
+        setScoreDisplay('none')
+
+        return onWinnerClick(loser,winner,matchIndex,roundIndex)
+
     }
 
     return (
 
         <>
 
-        <Container className="tournamentCard" style={played ? {opacity: 0.75} : null}>
+        <Container onClick={handleClick} className="tournamentCard" style={played ? {opacity: 0.75} : null}>
 
             {/* On click the opposite players id is handed up as the function looks
             to receive the player to eliminate*/}
 
             {/* The key is passed down as the match id*/}
 
-            <h4 onClick={() => handleClick(1)}>{player1}</h4>
+            <h4>{player1}</h4>
             <h4>VS</h4>
-            <h4 onClick={() => handleClick(2)}>{player2}</h4>
+            <h4>{player2}</h4>
 
         </Container>
 
         <ScoreInput 
             p1Name={player1} 
             p2Name={player2} 
-            display={scoreDisplay} 
+            display={scoreDisplay}
+            submitScore={submitScore} 
         />
 
         </>
