4a82677e84f29f1c7913b5fee9156c7c269805db
Barney Gammond <barney@gammond.co.uk>
2020-04-03 12:05:45 +0100

    Updates

---
+++ precache-manifest.bf315546f7d41e88b9f7fedeb58d404a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167f7a97b97435fcf5a1ade21d619aa7",
+    "url": "/table-tennis-tournament/index.html"
+  },
+  {
+    "revision": "8ad803d445f7376b8c43",
+    "url": "/table-tennis-tournament/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8cf5cfce2fd8b751c3fe",
+    "url": "/table-tennis-tournament/static/css/main.a03d2a5e.chunk.css"
+  },
+  {
+    "revision": "8ad803d445f7376b8c43",
+    "url": "/table-tennis-tournament/static/js/2.fc8bdc1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/table-tennis-tournament/static/js/2.fc8bdc1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf5cfce2fd8b751c3fe",
+    "url": "/table-tennis-tournament/static/js/main.0b95ac78.chunk.js"
+  },
+  {
+    "revision": "aa5a6e133debce55042d",
+    "url": "/table-tennis-tournament/static/js/runtime-main.00f4aefc.js"
+  },
+  {
+    "revision": "75365efdb746b3e14b37b4ab21fa6ed3",
+    "url": "/table-tennis-tournament/static/media/Quicksand-Medium.75365efd.ttf"
+  },
+  {
+    "revision": "316c232187bac46e708fad0b5d83666f",
+    "url": "/table-tennis-tournament/static/media/Quicksand-Regular.316c2321.ttf"
+  },
+  {
+    "revision": "ba0080fada3310c48a0983c0a081db8b",
+    "url": "/table-tennis-tournament/static/media/Signika-Regular.ba0080fa.ttf"
+  },
+  {
+    "revision": "b355f4bd338509eac11b9da595dbfdd4",
+    "url": "/table-tennis-tournament/static/media/Signika-SemiBold.b355f4bd.ttf"
+  }
+]);
